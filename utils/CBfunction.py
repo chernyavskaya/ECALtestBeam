f3409db01c76c5264cb8c05a72d2ab29e8876cc8
Nadezda <nadezda.chernyavskaya>
2019-07-31 09:28:12 +0200

    working ecal energy resolution

--- utils/CBfunction.py
+++ utils/CBfunction.py
@@ -6,12 +6,13 @@ import array as array
 
 class CBfunction:
     
-    nbins = 500
+    nbins = 600
     xmin = 0
-    xmax = 10000
+    xmax = 12000
     xaxis_scale = 0.2
+    a_initial = 0.5
+    n_initial = 7
 
-    
     def __init__(self,data):
         self.data = data ## data should be TChain already
       
@@ -27,6 +28,24 @@ class CBfunction:
     def set_selection(self):
         self.selection = "n_tracks==1 && fabs(X-(%.2f))<%.2f && fabs(Y-(%.2f))<%.2f"%(self.xcenter,self.window,self.ycenter,self.window)
 
+    def prepare_sumhistogram(self,dict_crystals_calibration,matrix):
+        self.set_selection()
+        self.hist = ROOT.TH1F("ampl_%s_%s"%(self.crystal,self.energy),"ampl_%s_%s"%(self.crystal,self.energy),self.nbins,self.xmin,self.xmax)
+
+        draw_function = '('
+        for enum,cryst in enumerate(matrix):
+            #draw_function +='fit_ampl[%s]*%.4f'%(cryst,dict_crystals_calibration[cryst])
+            draw_function +='((fit_ampl[%s] > 0) ? fit_ampl[%s] : amp_max[%s])*%.4f'%(cryst,cryst,cryst,dict_crystals_calibration[cryst])
+            if enum!=len(matrix)-1 : draw_function+='+'
+          #  else : draw_function+=")*%.4f>>ampl_%s_%s"%(dict_crystals_calibration['conversion_factor'],self.crystal,self.energy)
+            else : draw_function+=")>>ampl_%s_%s"%(self.crystal,self.energy)
+   
+    
+        self.data.Draw(draw_function,self.selection,"goff")
+        self.peak_position = self.hist.GetXaxis().GetBinCenter(self.hist.GetMaximumBin())
+        self.ymax_value = self.hist.GetMaximum()        
+        
+        
     def prepare_histogram(self):
         self.set_selection()
         self.hist = ROOT.TH1F("ampl_%s_%s"%(self.crystal,self.energy),"ampl_%s_%s"%(self.crystal,self.energy),self.nbins,self.xmin,self.xmax)        
@@ -40,8 +59,8 @@ class CBfunction:
         self.roohist = RooDataHist("roohist_fit_%s_%s"%(self.crystal,self.energy),"roohist_fit_%s_%s"%(self.crystal,self.energy),RooArgList(self.x),self.hist)
         self.m = RooRealVar("mean_%s_%s"%(self.crystal,self.energy),"mean_%s_%s"%(self.crystal,self.energy),self.peak_position,max(0.,self.peak_position*(1-self.xaxis_scale)),self.peak_position*(1+self.xaxis_scale))
         self.s = RooRealVar("sigma_%s_%s"%(self.crystal,self.energy),"sigma_%s_%s"%(self.crystal,self.energy),60,0,500)
-        self.a = RooRealVar("alpha_%s_%s"%(self.crystal,self.energy),"alpha_%s_%s"%(self.crystal,self.energy),0.5,-10.,10)
-        self.n = RooRealVar("exp_%s_%s"%(self.crystal,self.energy),"exp_%s_%s"%(self.crystal,self.energy),7,0.,150)
+        self.a = RooRealVar("alpha_%s_%s"%(self.crystal,self.energy),"alpha_%s_%s"%(self.crystal,self.energy),self.a_initial,-20.,20)
+        self.n = RooRealVar("exp_%s_%s"%(self.crystal,self.energy),"exp_%s_%s"%(self.crystal,self.energy),self.n_initial,0.,150)
         self.sig = RooCBShape("signal_%s_%s"%(self.crystal,self.energy),"signal_%s_%s"%(self.crystal,self.energy),self.x,self.m,self.s,self.a,self.n)
 
     def fitToData(self):
